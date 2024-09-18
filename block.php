a2a67945810cd360d5596918663c9300fcee8eb1
Lucas Troteseil <113380700+Lucas-tsl@users.noreply.github.com>
2024-09-18 11:32:12 +0200

    Create block.php
    Je m'initie à l'utilisation des différents blocs WordPress avec le thème Gutenberg, afin de créer des blocs personnalisés par la suite.

---
+++ block.php
@@ -0,0 +1,30 @@
+<!-- wp:query {"queryId":15,"query":{"perPage":10,"pages":0,"offset":"0","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-query alignwide" style="overflow-x: auto; white-space: nowrap; scroll-snap-type: x mandatory; scrollbar-width: none; -ms-overflow-style: none;">
+  <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+  <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:0;padding-bottom:var(--wp--preset--spacing--50);padding-left:0;">
+    <!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex"}} -->
+    <div class="wp-block-post-template" style="display: inline-block; width: calc(100% / 3); scroll-snap-align: start; margin-right: 20px;">
+      <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/4","style":{"spacing":{"margin":{"bottom":"0"},"padding":{"bottom":"var:preset|spacing|20"}}}} /-->
+      
+      <!-- wp:group {"style":{"spacing":{"blockGap":"10px","margin":{"top":"var:preset|spacing|20"},"padding":{"top":"0"}}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+      <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--20);padding-top:0;">
+        <!-- wp:post-title {"isLink":true,"fontSize":"large"} /-->
+        <!-- wp:post-excerpt {"moreText":"Lire la suite","textColor":"contrast-2","fontSize":"small"} /-->
+        <!-- wp:spacer {"height":"0px"} -->
+        <div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
+        <!-- /wp:spacer -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:post-template -->
+    
+    <!-- wp:query-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"space-between"}} -->
+    <div class="wp-block-query-pagination">
+      <!-- wp:query-pagination-previous /-->
+      <!-- wp:query-pagination-next /-->
+    </div>
+    <!-- /wp:query-pagination -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:query -->
